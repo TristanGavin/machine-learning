aa55e394c72f25541caa02476f26f569f66b24ff
Tristan Gavin <gavin.tristan@gmail.com>
2021-04-20 23:44:45 -0700

    finished implementing KNN and wrote report

--- hw2/knn.py
+++ hw2/knn.py
@@ -28,12 +28,14 @@ with open('test_pub.csv', mode='r') as file:
     for row in reader:
         global_test[next(cnt3)] = np.array(list(float(row[i]) for i in range(1,86)))
 
+
 # takes (list(keys),list(keys), dict, int)
-def evaluate_model(train, test, train_y, k):
+# loop through each row in test rows and get nearest neighbors, store accuracy. (only used when training model)
+def evaluate_model(train, test, train_y, k, t_on_t):
     classified = []
     accuracy = 0
     for row in test:
-        neighbors = get_kneighbors(train, row, k)
+        neighbors = get_kneighbors(train, row, k, t_on_t)
         prediction = make_prediction(neighbors, train_y)
         if train_y[row] == prediction:
             accuracy += 1
@@ -41,38 +43,50 @@ def evaluate_model(train, test, train_y, k):
 
 # get k nearest neighbors
 # takes (list(keys), int(key) to a row, k)
-# need to use broadcasting
-def get_kneighbors(train, test_row, k_neighbors):
+def get_kneighbors(train, test_row, k_neighbors, t_on_t):
     x = np.array(list(global_train[key] for key in train))
-    y = x-global_train[test_row]
+    #if training use x-train data otherwise find x-test 
+    if t_on_t == True:
+        y = x-global_train[test_row]
+    else:
+        y = x-global_test[test_row]
+
+    #calculate norm (use broadcasting to do whole row at a time)
     ynorm = np.linalg.norm(y, axis=1)
     distance = list((train[i], ynorm[i]) for i in range(len(train)))
     sorted_distance = sorted(distance, key=lambda item: item[1])
 
+    # create array of the k shortest distances
     neighbors = []
     for k in range(k_neighbors):
         neighbors.append(sorted_distance[k])
     return neighbors
 
+# take max of the k-nearest neighbor outputs and make that the prediction for this data point
 def make_prediction(neighbors, train_y):
     neighbor_output = list(train_y[n[0]] for n in neighbors)
     prediction = max(set(neighbor_output), key=neighbor_output.count)
     return prediction
 
-# # Make prediction
-# neighbors = get_kneighbors(train, test[0], 5)
-# neighbor_output = list(train_y[n[0]] for n in neighbors)
-# prediction = max(set(neighbor_output), key=neighbor_output.count)
+# function to run if evaluating test data
+def test_evaluation(train, test, k):
+    t_on_t = False # poorly named variable to tell program wether this is new test data, or if we are training.
+    prediction_list = []
+    for row in test:
+        neighbors = get_kneighbors(train, row, k, t_on_t)
+        prediction = make_prediction(neighbors, train_y)
+        prediction_list.append((int(row), int(prediction)))
+    return prediction_list
 
+# function to run to train on train.
 def train_on_train(train_keys,k):
-    evaluate_model(train_keys, train_keys, train_y, k)
+    t_on_t = True
+    evaluate_model(train_keys, train_keys, train_y, k, t_on_t)
 
-
-#K-Fold cross validation
-# randomly split train into 4 equal sized sets (randomly shuffle the ids in train)
-train_keys = list(global_train.keys())
-
-def k_fold(train_keys):
+# function to run if doing k_fold validation
+def k_fold(train_keys, kn):
+    t_on_t = True
+    # randomly split train into 4 equal sized sets (randomly shuffle the ids in train)
     random.shuffle(train_keys)
     fold1 = train_keys[:2000]
     fold2 = train_keys[2000:4000]
@@ -80,13 +94,24 @@ def k_fold(train_keys):
     fold4 = train_keys[6000:8000]
 
     folds = [fold1,fold2,fold3,fold4]
-
     for k in range(len(folds)):
         trainfold = sum(folds[:k], []) + sum(folds[k+1:], [])
         testfold = folds[k]
         # now evaluate model on testfold
-        evaluate_model(trainfold, testfold, train_y, 5)
+        evaluate_model(trainfold, testfold, train_y, kn, t_on_t)
 
 start_time = time.time()
-train_on_train(train_keys,999)
-print("--- %s seconds ---" % (time.time() - start_time))
+
+train_keys = list(global_train.keys())
+test_keys = list(global_test.keys())
+
+# k_fold(train_keys, 6000)
+# train_on_train(train_keys,8000)
+output = test_evaluation(train_keys, test_keys, 72)
+with open('submission.csv','w', newline='') as out:
+    csv_out = csv.writer(out)
+    csv_out.writerow(['id','income'])
+    for row in output:
+        csv_out.writerow(row)
+
+print("--- %s seconds ---" % (time.time() - start_time)) # just for tracking run time
